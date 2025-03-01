ef3372ac8d17cdc48583e97a07eafb50dc435bef
nitish-nks <nitishsinghz2000@gmail.com>
2025-03-01 15:33:17 +0530

    User now Can't provide blank and space as a name and email

--- src/main/java/com/example/dio/dto/request/RegistrationRequest.java
+++ src/main/java/com/example/dio/dto/request/RegistrationRequest.java
@@ -2,6 +2,7 @@ package com.example.dio.dto.request;
 
 import com.example.dio.enums.UserRole;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,10 +10,14 @@ import lombok.Setter;
 @Getter
 @Setter
 public class RegistrationRequest {
-    @NotNull
-    @NotBlank
+    @NotEmpty(message = "User name can't be null or blank")
+    @NotBlank(message = "You can't provide space as name")
     public String username;
+
+    @NotEmpty(message = "Email can't be blank")
+    @NotBlank(message = "You can't provide space as email")
     public String email;
+
     public String password;
     public long phNo;
     public UserRole role;
