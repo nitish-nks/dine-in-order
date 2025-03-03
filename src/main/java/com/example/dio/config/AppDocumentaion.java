4415193147271eba0e74ed539bf019b125cacb06
nitish-nks <nitishsinghz2000@gmail.com>
2025-03-03 17:10:29 +0530

    added the Class AppDocumEntaion for  Swager

--- src/main/java/com/example/dio/config/AppDocumentaion.java
+++ src/main/java/com/example/dio/config/AppDocumentaion.java
@@ -1,4 +1,34 @@
 package com.example.dio.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
 public class AppDocumentaion {
+
+    @Bean
+    OpenAPI openAPI() {
+        return new OpenAPI().info(info());
+    }
+
+    private Info info() {
+        return new Info().title("Dine in Order API")
+                .description("""
+                        ##Description
+                        **Dine In Order** is an Api built using spring boot REST Architecture.
+                        the API is designed to serve as a backend to a application that deals in ordering food online.
+                        
+                        ## Tech-Stack
+                        - Java 8
+                        - Spring Boot
+                        - Spring Data JPA
+                        - MySql Database
+                        - Spring Security
+                        """)
+                .version("v1");
+    }
 }
