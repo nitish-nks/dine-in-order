4bd9f151267adc4592e24e8f56dfdb50bfc59dc1
nitish-nks <nitishsinghz2000@gmail.com>
2025-03-04 16:38:31 +0530

    Updated the request mapping

--- src/main/java/com/example/dio/controller/UserController.java
+++ src/main/java/com/example/dio/controller/UserController.java
@@ -19,7 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("${app.base-url}")
 @AllArgsConstructor
 @Tag(name = "User Controller", description = "Collection API Endpoints Dealing with user data.")
 public class UserController {
