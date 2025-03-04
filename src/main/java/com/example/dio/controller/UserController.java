60fe09e8fdaf4bc2d2463a931959ff4dff2374f5
nitish-nks <nitishsinghz2000@gmail.com>
2025-03-04 15:55:19 +0530

    Modified the base url for versioning

--- src/main/java/com/example/dio/controller/UserController.java
+++ src/main/java/com/example/dio/controller/UserController.java
@@ -19,7 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/v1")
 @AllArgsConstructor
 @Tag(name = "User Controller", description = "Collection API Endpoints Dealing with user data.")
 public class UserController {
