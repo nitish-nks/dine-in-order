67fd37cbc40ab6ead5814094c6e5e7b9bc9b37d7
nitish-nks <nitishsinghz2000@gmail.com>
2025-03-05 17:15:06 +0530

    WE have added Mapper information

--- src/main/java/com/example/dio/mapper/RestaurantMapper.java
+++ src/main/java/com/example/dio/mapper/RestaurantMapper.java
@@ -1,4 +1,13 @@
 package com.example.dio.mapper;
 
+import com.example.dio.dto.request.RestaurantRequest;
+import com.example.dio.dto.response.RestaurantResponse;
+import com.example.dio.model.Restaurant;
+
 public interface RestaurantMapper {
+    Restaurant mapToRestaurantEntity(RestaurantRequest restaurantRequest);
+
+    RestaurantResponse mapToRestaurantResponse(Restaurant restaurant);
+
 }
+
