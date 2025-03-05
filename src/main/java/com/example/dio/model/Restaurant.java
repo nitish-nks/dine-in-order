e065c177735321270342ea7e655c450eec872b8f
nitish-nks <nitishsinghz2000@gmail.com>
2025-03-05 15:43:33 +0530

    Added a new entity as restaurant and mapped it with couisine and new enum is also added

---
+++ src/main/java/com/example/dio/model/Restaurant.java
@@ -0,0 +1,33 @@
+package com.example.dio.model;
+
+import com.example.dio.enums.DietType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
+public class Restaurant {
+    @Id
+    private long restaurantId;
+    private String name;
+    private String Address;
+    private long contactNumber;
+    private String contactEmail;
+    private LocalTime opensAt;
+    private LocalTime closeAt;
+    private DietType dietType;
+    private LocalTime createdAt;
+    private LocalTime modifiedAt;
+
+    @ManyToMany(mappedBy = "restaurantId", fetch = FetchType.EAGER)
+    private CuisineType cuisineType;
+
+
+}
