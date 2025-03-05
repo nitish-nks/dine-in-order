2e00a63faa63697c4501e4e5ae0a9d9ca2b064f4
nitish-nks <nitishsinghz2000@gmail.com>
2025-03-05 15:47:32 +0530

    Added mapping info between admin and restaurant

--- src/main/java/com/example/dio/model/Admin.java
+++ src/main/java/com/example/dio/model/Admin.java
@@ -1,9 +1,12 @@
 package com.example.dio.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "adims")
 public class Admin extends User {
+    @OneToMany
+    private Restaurant restaurant;
 }
--- src/main/java/com/example/dio/model/CuisineType.java
+++ src/main/java/com/example/dio/model/CuisineType.java
@@ -1,4 +1,20 @@
 package com.example.dio.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class CuisineType {
+    @Id
+    @Column(name = "cuisine_name")
+    private String cuisineName;
+
+    @ManyToMany
+    private Restaurant restaurant;
 }
