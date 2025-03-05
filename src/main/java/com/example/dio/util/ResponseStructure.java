1fd434b164eb7376c86096a35f96d2d676b9e704
nitish-nks <nitishsinghz2000@gmail.com>
2025-03-05 14:30:51 +0530

    Adeed JavaDoc for ResponseStructure documentation

--- src/main/java/com/example/dio/util/ResponseStructure.java
+++ src/main/java/com/example/dio/util/ResponseStructure.java
@@ -12,10 +12,31 @@ import org.springframework.http.HttpStatus;
 @NoArgsConstructor
 public class ResponseStructure<T> {
 
+    /**
+     * HTTP status code of the response.
+     */
     private int status;
+
+    /**
+     * A descriptive message about the response.
+     */
     private String message;
+
+    /**
+     * The data payload of the response.
+     */
     private T data;
 
+
+    /**
+     * Creates a new {@code ResponseStructure} instance with the given status, message, and data.
+     *
+     * @param status  the {@link HttpStatus} of the response
+     * @param message a descriptive message
+     * @param data    the data payload to be included in the response
+     * @param <T>     the type of the data payload
+     * @return a new {@code ResponseStructure} instance
+     */
     public static <T> ResponseStructure<T> create(HttpStatus status, String message, T data) {
         ResponseStructure<T> response = new ResponseStructure<>();
         response.status = status.value();
