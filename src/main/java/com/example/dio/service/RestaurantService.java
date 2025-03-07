b49408093586d4766387144fd2374a05b354976f
nitish-nks <nitishsinghz2000@gmail.com>
2025-03-07 22:54:49 +0530

    here we have added the add Restaurant service to the service class

--- src/main/java/com/example/dio/service/RestaurantService.java
+++ src/main/java/com/example/dio/service/RestaurantService.java
@@ -1,4 +1,9 @@
 package com.example.dio.service;
 
+import com.example.dio.dto.request.RestaurantRequest;
+import com.example.dio.dto.response.RestaurantResponse;
+
 public interface RestaurantService {
+    RestaurantResponse registerRestaurant(RestaurantRequest restaurantRequest, long userId);
+
 }
