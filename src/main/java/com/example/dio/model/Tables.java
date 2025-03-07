b1e351b515fd776dc868467238c401e8e9c4f3ba
nitish-nks <nitishsinghz2000@gmail.com>
2025-03-07 23:03:25 +0530

    here we have added the table entity for the restaurant

--- src/main/java/com/example/dio/model/Tables.java
+++ src/main/java/com/example/dio/model/Tables.java
@@ -1,7 +1,29 @@
 package com.example.dio.model;
 
-import jakarta.persistence.Entity;
+import com.example.dio.enums.TableStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-public class Table {
+@Table(name = "tables")
+@Getter
+@Setter
+public class Tables {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long tableId;
+
+    @Column(name = "TableNumber")
+    private long tableNo;
+
+    @Column(name = "TotalCapacity")
+    private long tableCapacity;
+
+    @Column(name = "TableStatus")
+    private TableStatus tableStatus;
+
+    @Column(name = "TableOfWhichRestaurant")
+    @ManyToOne
+    private Restaurant restaurant;
 }
