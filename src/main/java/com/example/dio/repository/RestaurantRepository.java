b765491bf697a8bc1eacf02f36ca56aa6c7f8b63
nitish-nks <nitishsinghz2000@gmail.com>
2025-03-07 22:58:28 +0530

    add UserRepository for data access

--- src/main/java/com/example/dio/repository/RestaurantRepository.java
+++ src/main/java/com/example/dio/repository/RestaurantRepository.java
@@ -1,4 +1,7 @@
 package com.example.dio.repository;
 
-public interface RestaurantRepository {
+import com.example.dio.model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 }
