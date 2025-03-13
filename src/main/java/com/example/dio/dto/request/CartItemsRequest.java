f1e52613ef79ca31516f917f37b4cf40302ca49a
nitish-nks <nitishsinghz2000@gmail.com>
2025-03-13 13:36:40 +0530

    Created the items which need to be cartItems request

--- src/main/java/com/example/dio/dto/request/CartItemsRequest.java
+++ src/main/java/com/example/dio/dto/request/CartItemsRequest.java
@@ -1,5 +1,12 @@
-package com.example.dio.controller;
+package com.example.dio.dto.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 public class CartItemsRequest {
+    @NotNull(message = "Food item ID is required")
+    private Long foodItemId;
 
+    @Min(value = 1, message = "Quantity must be at least 1")
+    private int quantity;
 }
